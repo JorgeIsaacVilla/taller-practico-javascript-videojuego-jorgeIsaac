d1c2120b3e0d5f68ad165a9879ac9d603e216d10
JorgeIsaacVilla <jvilla10@outlook.es>
2023-05-16 15:07:47 -0500

    clase 12 colitions

--- game.js
+++ game.js
@@ -8,32 +8,85 @@ const btnDown = document.querySelector("#down");
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+};
+
+const giftPosition = {
+    x: undefined,
+    y: undefined,
+};
+
 window.addEventListener("load", setCanvasSize); /*Esto ayuda para decirle al algoritmo que apenas termine de cargar el html, cargue ensequida este documento. */
 window.addEventListener("resize", setCanvasSize);  /*"resize" esta indicación le dice al algoritmo que cuando cambie de tamaño la pantalla (resize) recargue nuevamente el juego */
 
+
+function setCanvasSize () {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+    
+    canvas.setAttribute("width", canvasSize)
+    canvas.setAttribute("height", canvasSize)
+    
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
 
 
 function startGame(){
 /*para hacer el canvas responsive con buenas practicas podemos hacer lo siguiente: (inicio) */
 
-console.log({canvasSize, elementsSize});
+console.log({ canvasSize, elementsSize });
 
-game.font = elementsSize  + "px verdana";
-game.textAlign ="end";
+game.font = elementsSize + 'px Verdana';
+game.textAlign = 'end';
 
 const map = maps[0];
-const mapRows = map.trim().split("\n");
-const mapRowsCols = mapRows.map(row => row.trim().split(""));
-console.log(map, mapRows, mapRowsCols);
+const mapRows = map.trim().split('\n');
+const mapRowCols = mapRows.map(row => row.trim().split(''));
+console.log({map, mapRows, mapRowCols});
+
+game.clearRect(0,0,canvasSize, canvasSize);
+
+mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+
+      if (col == 'O') {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+          console.log({playerPosition});
+        }
+      } else if ( col == "I") {
+        giftPosition.x = posX;
+        giftPosition.y = posY;
+      }
+      
+      game.fillText(emoji, posX, posY);
+    });
+  });
+
+movePlayer();
+
 /*si yo uso el atributo .split lo que hara es partir la oración por fracies o silavas. ejemplo: si pongo en la consola //"hola mundo como estas".split(" ")// lo que hara es partirme la frace en palabras hola/mundo/como/estas/, pero si lo hago sin espacio en "" lo que hara es partirlo por cilavas // "hola mundo".split("")   -->> h.o.l.a. .m.u.n.d.o  todos en forma de arreglos*/
 
 /*el .trim nos ayuda a limpiar los espacios en blancos de la cadena de texto a la que se le utilizo .split, para limpiar los arrays */
 
+
+/*
 for (let row = 1; row<=10; row++){
     for (let col = 1; col <= 10; col++) {
         game.fillText(emojis[mapRowsCols[row - 1][col - 1]] , elementsSize * col, elementsSize  * row);
     }
-}
+}*/
 
 
 //canvas.setAttribute("width", window.innerWidth * 0.75); con este atributo, lo que hacemos es darle al width del vancas segun las propiedades del windows 0.75 para quitarle el 0.25 y no ocupe la totalidad de la pantalla
@@ -55,22 +108,21 @@ for (let row = 1; row<=10; row++){
     //game.fillText("platzi",25,25);//game.fillText(Texto,100,100); /*game.fillText es un metodo de la API de dibujo en canvas en html5 para insertar textos dentro del canvas.(x, y, width, height) son los parametros dentro del objeto dentro del canvas;  */
 }
 
-function setCanvasSize () {
-    let canvasSize;
 
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.4;
-    } else {
-        canvasSize = window.innerWidth * 0.4;
+
+function movePlayer() {
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
+    
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+
+    if(giftCollision){
+        console.log("subiste de nvl")
     }
-    
-    canvas.setAttribute("width", canvasSize)
-    canvas.setAttribute("height", canvasSize)
-    
-    elementsSize = canvasSize / 10;
 
-    startGame();
-}
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+  }
 
 
 /*para escuchar los eventos de los botones del teclado */
@@ -87,18 +139,41 @@ function moveBykeys (event) {
         else if (event.key == "ArrowLeft") moveLeft(); 
         else if (event.key == "ArrowRight") moveRight();  
         else if (event.key == "ArrowDown") moveDown();
-
 }
 
 function moveUP(){
 
+    if((playerPosition.y - elementsSize) < 0){
+        console.log("OUT")
+    } else {
+        playerPosition.y -= elementsSize;
+        startGame();
+    }
+
 }
 function moveLeft(){
-    
+    if((playerPosition.x - elementsSize) < elementsSize){
+        console.log("OUT")
+    } else {
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
 }
 function moveRight(){
-    
+
+    if((playerPosition.x + elementsSize)> (canvasSize + 40)){
+        console.log("OUT");
+    }else{
+        playerPosition.x += elementsSize;
+        startGame();
+        
+    }
 }
 function moveDown(){
-    
+    if((playerPosition.y + elementsSize) > canvasSize){
+        console.log("OUT");
+    }else{
+    playerPosition.y += elementsSize;
+    startGame();
+    }
 }
